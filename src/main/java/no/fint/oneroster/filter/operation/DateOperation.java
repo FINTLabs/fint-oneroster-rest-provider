5a8289c1db793f7767c92f8e33a6cc690c658b79
torgravels <torleif@fintlabs.no>
2020-03-11 11:51:38 +0100

    ZonedDate to LocalDate cast

--- src/main/java/no/fint/oneroster/filter/operation/DateOperation.java
+++ src/main/java/no/fint/oneroster/filter/operation/DateOperation.java
@@ -3,38 +3,39 @@ package no.fint.oneroster.filter.operation;
 import no.fint.oneroster.filter.operand.Operand;
 
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 public class DateOperation implements Operation {
 
     @Override
     public Boolean eq(Operand leftOperand, Operand rightOperand) {
-        return LocalDate.parse(leftOperand.getObjectValue().toString()).isEqual(rightOperand.getDateValue());
+        return ZonedDateTime.parse(leftOperand.getObjectValue().toString()).toLocalDate().isEqual(rightOperand.getDateValue());
     }
 
     @Override
     public Boolean ne(Operand leftOperand, Operand rightOperand) {
-        return !LocalDate.parse(leftOperand.getObjectValue().toString()).isEqual(rightOperand.getDateValue());
+        return !ZonedDateTime.parse(leftOperand.getObjectValue().toString()).toLocalDate().isEqual(rightOperand.getDateValue());
 
     }
 
     @Override
     public Boolean gt(Operand leftOperand, Operand rightOperand) {
-        return LocalDate.parse(leftOperand.getObjectValue().toString()).compareTo(rightOperand.getDateValue()) > 0;
+        return ZonedDateTime.parse(leftOperand.getObjectValue().toString()).toLocalDate().compareTo(rightOperand.getDateValue()) > 0;
     }
 
     @Override
     public Boolean lt(Operand leftOperand, Operand rightOperand) {
-        return LocalDate.parse(leftOperand.getObjectValue().toString()).compareTo(rightOperand.getDateValue()) < 0;
+        return ZonedDateTime.parse(leftOperand.getObjectValue().toString()).toLocalDate().compareTo(rightOperand.getDateValue()) < 0;
     }
 
     @Override
     public Boolean ge(Operand leftOperand, Operand rightOperand) {
-        return LocalDate.parse(leftOperand.getObjectValue().toString()).compareTo(rightOperand.getDateValue()) >= 0;
+        return ZonedDateTime.parse(leftOperand.getObjectValue().toString()).toLocalDate().compareTo(rightOperand.getDateValue()) >= 0;
     }
 
     @Override
     public Boolean le(Operand leftOperand, Operand rightOperand) {
-        return LocalDate.parse(leftOperand.getObjectValue().toString()).compareTo(rightOperand.getDateValue()) <= 0;
+        return ZonedDateTime.parse(leftOperand.getObjectValue().toString()).toLocalDate().compareTo(rightOperand.getDateValue()) <= 0;
     }
 
     @Override
