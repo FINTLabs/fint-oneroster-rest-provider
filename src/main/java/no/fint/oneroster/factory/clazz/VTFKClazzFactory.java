ff182bada847378b7ea524b99986cdcefcc5660f
torgravels <torleif@fintlabs.no>
2020-04-27 20:55:29 +0200

    School mapping table

--- src/main/java/no/fint/oneroster/factory/clazz/VTFKClazzFactory.java
+++ src/main/java/no/fint/oneroster/factory/clazz/VTFKClazzFactory.java
@@ -12,12 +12,13 @@ import no.fint.oneroster.model.vocab.ClazzType;
 import no.fint.oneroster.model.vocab.GUIDType;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class VTFKClazzFactory implements ClazzFactory {
-    private final String PREFIX = "OF";
-    private final String SUFFIX = "Klasse";
     private final String SEPARATOR = "-";
 
     public Clazz basisGroup(BasisgruppeResource basisgruppeResource, ArstrinnResource arstrinnResource, SkoleResource skoleResource, List<AcademicSession> terms) {
@@ -47,13 +48,44 @@ public class VTFKClazzFactory implements ClazzFactory {
     }
 
     public String basisGroupNameConverter(String name) {
-        String between = StringUtils.substringBetween(name,"_", "@");
-        return PREFIX +
+        String between = StringUtils.substringBetween(name, "_", "@");
+        String school = StringUtils.substringAfterLast(between, "_");
+        String group = StringUtils.substringBeforeLast(between, "_");
+
+        return schools.getOrDefault(school, school) +
                 SEPARATOR +
-                StringUtils.chop(StringUtils.substringAfterLast(between, "_")) +
+                group +
                 SEPARATOR +
-                StringUtils.substringBeforeLast(between, "_") +
-                SEPARATOR +
-                SUFFIX;
+                "Klasse";
     }
+
+    private static final Map<String, String> schools = Stream.of(
+            new AbstractMap.SimpleImmutableEntry<>("BAMVS", "OF-BAV"),
+            new AbstractMap.SimpleImmutableEntry<>("BOEVS", "OF-BOV"),
+            new AbstractMap.SimpleImmutableEntry<>("FRVS", "OF-FRV"),
+            new AbstractMap.SimpleImmutableEntry<>("GRVS", "OF-GRV"),
+            new AbstractMap.SimpleImmutableEntry<>("KLOVS", "OF-HJV"),
+            new AbstractMap.SimpleImmutableEntry<>("HLVS", "OF-HOLV"),
+            new AbstractMap.SimpleImmutableEntry<>("HVS", "OF-HORV"),
+            new AbstractMap.SimpleImmutableEntry<>("HVS-NETT", "OF-NETT"),
+            new AbstractMap.SimpleImmutableEntry<>("KBV", "OF-KB"),
+            new AbstractMap.SimpleImmutableEntry<>("KRAVS", "OF-KRV"),
+            new AbstractMap.SimpleImmutableEntry<>("MVS", "OF-MEV"),
+            new AbstractMap.SimpleImmutableEntry<>("NOMVS", "OF-NOMV"),
+            new AbstractMap.SimpleImmutableEntry<>("NOTVS", "OF-NOV"),
+            new AbstractMap.SimpleImmutableEntry<>("NVS", "OF-NTV"),
+            new AbstractMap.SimpleImmutableEntry<>("PORVS", "OF-POV"),
+            new AbstractMap.SimpleImmutableEntry<>("RVS", "OF-REV"),
+            new AbstractMap.SimpleImmutableEntry<>("RJUVS", "OF-RJV"),
+            new AbstractMap.SimpleImmutableEntry<>("SVS", "OF-SANV"),
+            //new AbstractMap.SimpleImmutableEntry<>("SFVS", "OF-SFH"), Sandefjord folkehøyskole
+            new AbstractMap.SimpleImmutableEntry<>("SFVS", "OF-SFV"),
+            new AbstractMap.SimpleImmutableEntry<>("SKIVS", "OF-SKIV"),
+            new AbstractMap.SimpleImmutableEntry<>("SKOVS", "OF-SKOV"),
+            new AbstractMap.SimpleImmutableEntry<>("MVS-SMIH", "OF-SMI"),
+            new AbstractMap.SimpleImmutableEntry<>("THVS", "OF-THV"),
+            new AbstractMap.SimpleImmutableEntry<>("DALVS", "OF-VTV"),
+            new AbstractMap.SimpleImmutableEntry<>("TELVS", "NIK-FAGS"),
+            new AbstractMap.SimpleImmutableEntry<>("FIV", "NIK-FAGS"))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 }
