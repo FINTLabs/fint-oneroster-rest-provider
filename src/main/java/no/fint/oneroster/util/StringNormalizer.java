3e54c66e54271bb13418276dbe77f5e9bafa3bec
torgravels <torleif@fintlabs.no>
2020-05-05 20:59:30 +0200

    Update StringNormalizer.java

--- src/main/java/no/fint/oneroster/util/StringNormalizer.java
+++ src/main/java/no/fint/oneroster/util/StringNormalizer.java
@@ -1,12 +1,14 @@
 package no.fint.oneroster.util;
 
 import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 public class StringNormalizer {
+    private static final Pattern pattern = Pattern.compile("[^\\p{ASCII}]");
 
     public static String normalize(String text) {
         return Normalizer
                 .normalize(text, Normalizer.Form.NFD)
-                .replaceAll("[^\\p{ASCII}]", "");
+                .replaceAll(pattern.pattern(), "");
     }
 }
