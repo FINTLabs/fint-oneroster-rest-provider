3b9a96e8794c16bda2a79bf41bc31c700b6700e4
torgravels <torleif@fintlabs.no>
2020-05-05 21:40:19 +0200

    Since timestamp

--- src/main/java/no/fint/oneroster/repository/FintRepository.java
+++ src/main/java/no/fint/oneroster/repository/FintRepository.java
@@ -10,9 +10,14 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
@@ -24,6 +29,8 @@ public class FintRepository {
     private final Authentication principal;
     private final OAuth2AuthorizedClientManager authorizedClientManager;
     private final FintProperties fintProperties;
+
+    private final Map<String, Long> sinceTimestamp = new ConcurrentHashMap<>();
 
     public FintRepository(WebClient webClient, Authentication principal, OAuth2AuthorizedClientManager authorizedClientManager, FintProperties fintProperties) {
         this.webClient = webClient;
@@ -53,11 +60,20 @@ public class FintRepository {
 
         OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
 
-        return webClient.get()
-                .uri(endpoint)
+        URI uri = UriComponentsBuilder.fromHttpUrl(endpoint)
+                .queryParam("sinceTimeStamp", sinceTimestamp.getOrDefault(endpoint, 0L))
+                .build()
+                .toUri();
+
+        Mono<T> resources = webClient.get()
+                .uri(uri)
                 .attributes(oauth2AuthorizedClient(authorizedClient))
                 .retrieve()
                 .bodyToMono(clazz)
-                .doOnSuccess(it -> log.info("Updated {}...", endpoint));
+                .doOnSuccess(it -> log.info("Updated {}... ", uri));
+
+        sinceTimestamp.put(endpoint, Instant.now().toEpochMilli());
+
+        return resources;
     }
 }
