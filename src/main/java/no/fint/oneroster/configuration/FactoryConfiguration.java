f9f22a81b49cee6c0fce3dee946ae464d99f07ba
torgravels <torleif@fintlabs.no>
2021-02-25 15:56:03 +0100

    Cleanup custom factories

--- src/main/java/no/fint/oneroster/configuration/FactoryConfiguration.java
+++ src/main/java/no/fint/oneroster/configuration/FactoryConfiguration.java
@@ -2,8 +2,7 @@ package no.fint.oneroster.configuration;
 
 import no.fint.oneroster.factory.clazz.ClazzFactory;
 import no.fint.oneroster.factory.clazz.DefaultClazzFactory;
-import no.fint.oneroster.factory.clazz.MRFylkeClazzFactory;
-import no.fint.oneroster.factory.clazz.VTFKClazzFactory;
+import no.fint.oneroster.factory.clazz.MrfylkeClazzFactory;
 import no.fint.oneroster.factory.user.DefaultUserFactory;
 import no.fint.oneroster.factory.user.ItslearningUserFactory;
 import no.fint.oneroster.factory.user.UserFactory;
@@ -15,43 +14,25 @@ import org.springframework.context.annotation.Configuration;
 public class FactoryConfiguration {
 
     @Bean
-    @ConditionalOnProperty(
-            value = "oneroster.profile.clazz-factory",
-            havingValue = "default",
-            matchIfMissing = true)
+    @ConditionalOnProperty(value = "oneroster.profile.clazz-factory", havingValue = "default", matchIfMissing = true)
     public ClazzFactory clazzFactory() {
         return new DefaultClazzFactory();
     }
 
     @Bean
-    @ConditionalOnProperty(
-            value = "oneroster.profile.clazz-factory",
-            havingValue = "vtfk")
-    public ClazzFactory vtfkClazzFactory() {
-        return new VTFKClazzFactory();
-    }
-
-    @Bean
-    @ConditionalOnProperty(
-            value = "oneroster.profile.clazz-factory",
-            havingValue = "mrfylke")
+    @ConditionalOnProperty(value = "oneroster.profile.clazz-factory", havingValue = "mrfylke")
     public ClazzFactory mrfylkeClazzFactory() {
-        return new MRFylkeClazzFactory();
+        return new MrfylkeClazzFactory();
     }
 
     @Bean
-    @ConditionalOnProperty(
-            value = "oneroster.profile.user-factory",
-            havingValue = "default",
-            matchIfMissing = true)
+    @ConditionalOnProperty(value = "oneroster.profile.user-factory", havingValue = "default", matchIfMissing = true)
     public UserFactory userFactory() {
         return new DefaultUserFactory();
     }
 
     @Bean
-    @ConditionalOnProperty(
-            value = "oneroster.profile.user-factory",
-            havingValue = "itslearning")
+    @ConditionalOnProperty(value = "oneroster.profile.user-factory", havingValue = "itslearning")
     public UserFactory itslearningUserFactory() {
         return new ItslearningUserFactory();
     }
