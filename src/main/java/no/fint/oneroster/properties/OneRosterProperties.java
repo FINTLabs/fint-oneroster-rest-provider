8ad1ce62247a5c67ccbbc1fa419a80fc67cf0fc3
Trond Magnus Sevre <trond@fintlabs.no>
2023-01-24 14:31:45 +0100

    Add possibility to filter by Personalressurskategori

--- src/main/java/no/fint/oneroster/properties/OneRosterProperties.java
+++ src/main/java/no/fint/oneroster/properties/OneRosterProperties.java
@@ -14,6 +14,7 @@ public class OneRosterProperties {
     private boolean contactTeacherGroups;
     private boolean parents;
     private boolean accessControl;
+    private String ignoredPersonalressurskategori;
     private Map<String, Client> clients;
 
     @Data
